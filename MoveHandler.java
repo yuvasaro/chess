272525f54bf2e9cb15e5a931c12d3735ea555fd7
Yuva Saro <56562141+yuvasaro@users.noreply.github.com>
2023-02-27 20:20:24 -0800

    Added a MoveHandler class

---
+++ MoveHandler.java
@@ -0,0 +1,41 @@
+/**
+ * A class that handles chess move input
+ */
+public class MoveHandler {
+    /* Indices of components of the move notation in a list returned by parseMove() */
+
+    // String representation of the piece being moved
+    private static final int PIECE_INDEX = 0;
+
+    /* In case two of the same pieces can move to the same square, we specify
+    with a letter or number the piece on a certain row or column (for example,
+    "Rad1" means "Rook on column a to square d1" 
+    
+    This string stores that letter or number */
+    private static final int PIECE_SPECIFIER_INDEX = 1;
+
+    // 2 character string (ex. "e4")
+    private static final int SQUARE_INDEX = 2;
+
+    // + or #
+    private static final int CHECK_MATE_INDEX = 3;
+
+    /**
+     * Uses regular expressions to extract the piece and square (and check/
+     * checkmate) of the move
+     */
+    public static String[] parseMove() {
+        // TODO
+        return null;
+    }
+
+    /**
+     * Converts a string representation of a square to board coordinates
+     * @param square the square string
+     * @return the coordinates of the given square on the board
+     */
+    public static int[] toCoords(String square) {
+        // TODO
+        return null;
+    }
+}
