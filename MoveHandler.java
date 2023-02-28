1fffb992b909f6aee1065384dc501dd2c0815add
Yuva Saro <56562141+yuvasaro@users.noreply.github.com>
2023-02-27 21:03:02 -0800

    Made move component indices public in MoveHandler

--- MoveHandler.java
+++ MoveHandler.java
@@ -9,20 +9,20 @@ public class MoveHandler {
     Format: ["piece", "letter/number (or null)", "square", "+/# (or null)"] */
 
     // String representation of the piece being moved
-    private static final int PIECE_INDEX = 0;
+    public static final int PIECE_INDEX = 0;
 
     /* In case two of the same pieces can move to the same square, we specify
     with a letter or number the piece on a certain row or column (for example,
     "Rad1" means "Rook on column a to square d1" 
     
     This string stores that letter or number */
-    private static final int PIECE_SPECIFIER_INDEX = 1;
+    public static final int PIECE_SPECIFIER_INDEX = 1;
 
     // 2 character string (ex. "e4")
-    private static final int SQUARE_INDEX = 2;
+    public static final int SQUARE_INDEX = 2;
 
     // + or #
-    private static final int CHECK_MATE_INDEX = 3;
+    public static final int CHECK_MATE_INDEX = 3;
 
     /**
      * Uses regular expressions to extract the piece and square (and check/
