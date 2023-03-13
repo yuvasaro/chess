7aff3de716fee74930e3f67b3fe35aca69872a4d
Your Name <you@example.com>
2023-03-13 13:47:46 -0700

    Added castle notation regex pattern to MoveHandler

--- MoveHandler.java
+++ MoveHandler.java
@@ -11,31 +11,38 @@ public class MoveHandler {
     Format: [piece (null for pawn), letter/number (or null), x or null, square, =<piece promoted to> or null, +/# (or null)] */
 
     // String representation of the piece being moved
-    public static final int PIECE_INDEX = 0;
+    public static final int PIECE = 0;
 
     /* In case two of the same pieces can move to the same square, we specify
     with a letter or number the piece on a certain row or column (for example,
     "Rad1" means "Rook on column a to square d1" 
     
     This string stores that letter or number */
-    public static final int SPECIFIER_INDEX = 1;
+    public static final int SPECIFIER = 1;
 
     // Capture - "x" or null
-    public static final int CAPTURE_INDEX = 2;
+    public static final int CAPTURE = 2;
 
     // 2 character string (ex. "e4")
-    public static final int SQUARE_INDEX = 3;
+    public static final int SQUARE = 3;
 
     // Promotion (ex. "=Q")
-    public static final int PROMOTION_INDEX = 4;
+    public static final int PROMOTION = 4;
+
+    // Castle (ex. "O-O")
+    public static final int CASTLE = 5;
 
     // + or #
-    public static final int CHECK_MATE_INDEX = 5;
+    public static final int CHECK_MATE = 6;
 
     // Regex pattern
     public static final String PATTERN = 
         "(^[NBRQK])?([a-h])?(x)?([a-h][1-8])(?:([=][NBQR]))?([+#])?";
 
+    // Castle regex pattern
+    public static final String CASTLE_PATTERN = "(O-O)(-O)?([+#])?";
+
+    // Map of letters to coordinates
     public static final Map<String, Integer> letterCoordMapping = Map.of(
         "a", 0,
         "b", 1,
@@ -55,18 +62,29 @@ public class MoveHandler {
         // Move ex. Ndxc7+ (Knight on d takes c7 check)
         // Piece = "N", specifier = "d", capture = "x", square = "c7", 
         // promotion = null, check/mate = "+"
-        String[] moveComponents = new String[6];
+        String[] moveComponents = new String[7];
         Pattern pattern = Pattern.compile(PATTERN);
+        Pattern castlePattern = Pattern.compile(CASTLE_PATTERN);
         Matcher matcher = pattern.matcher(move);
+        Matcher castleMatcher = castlePattern.matcher(move);
 
-        // Add move components to list
-        while (matcher.find()) {
-            moveComponents[PIECE_INDEX] = matcher.group(1);
-            moveComponents[SPECIFIER_INDEX] = matcher.group(2);
-            moveComponents[CAPTURE_INDEX] = matcher.group(3);
-            moveComponents[SQUARE_INDEX] = matcher.group(4);
-            moveComponents[PROMOTION_INDEX] = matcher.group(5);
-            moveComponents[CHECK_MATE_INDEX] = matcher.group(6);
+        // Castle pattern takes priority
+        if (castleMatcher.find()) {
+            String castleString = castleMatcher.group(1); // O-O
+            if (castleMatcher.group(2) != null) { // O-O-O
+                castleString += castleMatcher.group(2);
+            }
+            moveComponents[CASTLE] = castleString;
+            moveComponents[CHECK_MATE] = castleMatcher.group(3);
+        } 
+        // Else regular move components
+        else if (matcher.find()) {
+            moveComponents[PIECE] = matcher.group(1);
+            moveComponents[SPECIFIER] = matcher.group(2);
+            moveComponents[CAPTURE] = matcher.group(3);
+            moveComponents[SQUARE] = matcher.group(4);
+            moveComponents[PROMOTION] = matcher.group(5);
+            moveComponents[CHECK_MATE] = matcher.group(6);
         }
 
         return moveComponents;
@@ -119,11 +137,10 @@ public class MoveHandler {
         for (String component : move) {
             System.out.println(component);
         }
-
         System.out.println();
-
-        Point coords = toCoords("a1");
-        System.out.println(coords.x + ", " + coords.y);
-        System.out.println(toSquare(coords));
+        move = parseMove("O-O-O#");
+        for (String component : move) {
+            System.out.println(component);
+        }
     }
 }
